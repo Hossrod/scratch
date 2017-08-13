a88ef975a98e899e7be7922f87cb259f61545d0a
Hoss <hossrod@gmail.com>
2017-08-13 16:18:16 -0700

    Added code commenting.

--- raspberry/python/modules/PiInfo/PiInfoExample.py
+++ raspberry/python/modules/PiInfo/PiInfoExample.py
@@ -4,15 +4,22 @@ import PiInfo
 piInfo = PiInfo.PiInfo()
 logFile = "PiInfo.csv"
 
+# Create csv file (with header row) to log to
 with open(logFile, 'w') as file:
     file.write("ºC,ºF,Core (v),SDRAM Core (v),SDRAM IO (v),SDRAM PHY (v)\n")
 
+# Loop forever printing out PiInfo to console and CSV log file.
 while 1:
+    # Print info to console.
     print("CPU:                " + "{0:.1f}".format(piInfo.cpuTempC) + "ºC, " + "{0:.1f}".format(piInfo.cpuTempF) + "ºF")
     print("Core Voltage:       " + str(piInfo.coreVoltage))
     print("SDRAM Core Voltage: " + str(piInfo.sdramCoreVoltage))
     print("SDRAM IO Voltage:   " + str(piInfo.sdramIOVoltage))
     print("SDRAM PHY Voltage:  " + str(piInfo.sdramPHYVoltage) + "\n")
+    
+    # Append new row of info to csv log file.
     with open(logFile, 'a') as file:
         file.write(str(piInfo.cpuTempC) + "," + str(piInfo.cpuTempF) + "," + str(piInfo.coreVoltage) + "," + str(piInfo.sdramCoreVoltage) + "," + str(piInfo.sdramIOVoltage) + "," + str(piInfo.sdramPHYVoltage) + "\n")
+    
+    # Sleep for specified time before getting new info to print/log.
     time.sleep(5.0)
