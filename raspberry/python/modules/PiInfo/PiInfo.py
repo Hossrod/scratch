edbde7feb8d6158702ad1eb03e441502e5e3f2ae
Hossrod <hossrod@gmail.com>
2017-08-14 02:32:31 -0700

    Made path of vcgencmd a variable.

--- raspberry/python/modules/PiInfo/PiInfo.py
+++ raspberry/python/modules/PiInfo/PiInfo.py
@@ -1,5 +1,6 @@
 """
-Module provides classes and methods for getting information about the running Raspberry Pi.
+Module provides classes and methods for getting information about the running
+Raspberry Pi.
 
 Notes:
   ~ vcgencmd info: http://elinux.org/RPI_vcgencmd_usage
@@ -8,37 +9,43 @@ Notes:
 
 import subprocess
 
+
 class PiInfo:
+
+    _vcgencmdPath = '/opt/vc/bin/vcgencmd'
 
     def __vgcVolts(self, voltName):
         return float(subprocess
-                     .check_output(['/opt/vc/bin/vcgencmd', 'measure_volts', voltName])
+                     .check_output([self._vcgencmdPath,
+                         'measure_volts',
+                         voltName])
                      .decode('utf-8')
                      .replace("volt=", "")
                      .replace("V", ""))
-    
+
     def __getTempC(self):
         return float(subprocess
-                     .check_output(['/opt/vc/bin/vcgencmd', 'measure_temp'])
+                     .check_output([self._vcgencmdPath,
+                         'measure_temp'])
                      .decode('utf-8')
                      .replace("temp=", "")
                      .replace("'C", ""))
-    
+
     def __getTempF(self):
         return self.__getTempC() * 9 / 5 + 32
-    
+
     def __getCoreV(self):
         return self.__vgcVolts('core')
-    
+
     def __getSdramCoreV(self):
         return self.__vgcVolts('sdram_c')
-    
+
     def __getSdramIoV(self):
         return self.__vgcVolts('sdram_i')
-    
+
     def __getSdramPhyV(self):
         return self.__vgcVolts('sdram_p')
-    
+
     cpuTempC = property(__getTempC)
     cpuTempF = property(__getTempF)
     coreVoltage = property(__getCoreV)
--- raspberry/python/modules/PiInfo/PiInfoExample.py
+++ raspberry/python/modules/PiInfo/PiInfoExample.py
@@ -5,21 +5,27 @@ piInfo = PiInfo.PiInfo()
 logFile = "PiInfo.csv"
 
 # Create csv file (with header row) to log to
-with open(logFile, 'w') as file:
-    file.write("ºC,ºF,Core (v),SDRAM Core (v),SDRAM IO (v),SDRAM PHY (v)\n")
+with open(logFile, 'w') as fi:
+    fi.write("ºC,ºF,Core (v),SDRAM Core (v),SDRAM IO (v),SDRAM PHY (v)\n")
 
 # Loop forever printing out PiInfo to console and CSV log file.
 while 1:
     # Print info to console.
-    print("CPU:                " + "{0:.1f}".format(piInfo.cpuTempC) + "ºC, " + "{0:.1f}".format(piInfo.cpuTempF) + "ºF")
+    print("CPU Temp ºC:        " + str(piInfo.cpuTempC))
+    print("CPU Temp ºF:        " + str(piInfo.cpuTempF))
     print("Core Voltage:       " + str(piInfo.coreVoltage))
     print("SDRAM Core Voltage: " + str(piInfo.sdramCoreVoltage))
     print("SDRAM IO Voltage:   " + str(piInfo.sdramIOVoltage))
     print("SDRAM PHY Voltage:  " + str(piInfo.sdramPHYVoltage) + "\n")
-    
+
     # Append new row of info to csv log file.
-    with open(logFile, 'a') as file:
-        file.write(str(piInfo.cpuTempC) + "," + str(piInfo.cpuTempF) + "," + str(piInfo.coreVoltage) + "," + str(piInfo.sdramCoreVoltage) + "," + str(piInfo.sdramIOVoltage) + "," + str(piInfo.sdramPHYVoltage) + "\n")
-    
+    with open(logFile, 'a') as fi:
+        fi.write(str(piInfo.cpuTempC) + ","
+        + str(piInfo.cpuTempF) + ","
+        + str(piInfo.coreVoltage) + ","
+        + str(piInfo.sdramCoreVoltage) + ","
+        + str(piInfo.sdramIOVoltage) + ","
+        + str(piInfo.sdramPHYVoltage) + "\n")
+
     # Sleep for specified time before getting new info to print/log.
     time.sleep(5.0)
